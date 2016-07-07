902add5b3e13276e2450e22a101864f973fcc169
hoorayinteractive <hoorayinteractiveco@gmail.com>
2016-07-07 06:32:36 +0000

    commit04

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">LaraTestAgain 5</div>
+                <div class="title">LaraTestAgainAgain 5</div>
             </div>
         </div>
     </body>
